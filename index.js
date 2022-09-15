00e24a499dc68573cae1791df7b40ea69de728b1
mvonpieschel <105995667+mvonpieschel@users.noreply.github.com>
2022-09-14 21:09:21 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,69 @@
-function preguntas () {
-  let precio = parseInt(prompt("Ingresa el precio del producto que seleccionaste"));
-  let meses = parseInt(prompt("Ingresa la cantidad de cuotas sin interes que queres (3, 6 o 12)"));
-  dividir(precio, meses);
+const productos = [
+  {nombre: "cuadro1", precio: 150},
+  {nombre: "cuadro2" , precio: 300},
+  {nombre: "cuadro3" , precio: 450},
+  {nombre: "cuadro4" , precio: 600},
+  {nombre: "cuadro5" , precio: 700},
+  {nombre: "cuadro6" , precio: 800},
+];
+let carrito = []
+let seleccion = prompt("Hola ¿Queres agregar algun cuadros 3d a tu carrito? si o no.")
+
+while(seleccion.toLocaleLowerCase() != "si" && seleccion.toLocaleLowerCase() != "no"){
+  alert("Por favor ingrese si o no")
+  seleccion = prompt("Hola ¿Desea comprar algo? si o no.")
 }
-function dividir(num1, num2){
-  if (num2 == 3 || num2 == 6 || num2 == 12 ){
-    let numeros = num1/num2;
-    let decimales = numeros.toFixed(2);
-    alert("El precio a pagar por mes es de " + decimales + "$");
+
+if(seleccion == "si"){
+  alert("Estos son nuestros cuadros 3d disponibles")
+  let todosLosProductos = productos.map((producto) => `${producto.nombre}  $${producto.precio}`);
+  alert(todosLosProductos.join(" - "))
+}else if(seleccion == "no"){
+  alert("Gracias por la visita")
+}
+while(seleccion != "no"){
+  let producto = prompt("Agrega un producto a tu carrito")
+  let precio = 0
+  
+  if(producto == "cuadro1" || producto == "cuadro2" || producto == "cuadro3" || producto == "cuadro4" || producto == "cuadro5" || producto == "cuadro6"){
+      switch(producto){
+          case "cuadro1":
+              precio = 150 ;
+              break;
+          case "cuadro2":
+              precio = 300;
+              break;
+          case "cuadro3":
+              precio = 450 ;
+              break;
+          case "cuadro4":
+              precio = 600 ;
+              break;
+          case "cuadro5":
+              precio = 700 ;
+              break;
+          case "cuadro6":
+              precio = 800;
+              break;
+      }
+      let unidades =parseInt(prompt("¿Cuantos queres llevar?"))
+      carrito.push({producto, unidades, precio})
+      console.log(carrito)
   }else{
-    alert("La cantidad de cuotas seleccionadas es incorrecta, solo puede ser 3/6/12");
-    preguntas();
+      alert("No tenemos ese producto")
+  }
+  seleccion = prompt("Desea seguir comprando? si o no.")
+  while(seleccion.toLocaleLowerCase() != "si" && seleccion.toLocaleLowerCase() != "no"){
+      alert("Por favor ingrese si o no")
+      seleccion = prompt("Desea seguir comprando? si o no.")
+  }
+  while(seleccion.toLocaleLowerCase() === "no"){
+      alert("Gracias, hasta luego")
+      carrito.forEach((carritoFinal) => {
+          console.log(`producto : ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, total a pagar por producto: ${carritoFinal.unidades * carritoFinal.precio}`)
+      })
+      break
   }
 }
-
-let precio = parseInt(prompt("Ingresa el precio del producto que seleccionaste"));
-let meses = parseInt(prompt("Ingresa la cantidad de cuotas sin interes que queres (3, 6 o 12)"));
-
-dividir(precio, meses);
+const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0)
+console.log(`el total a pagar es: ${total}`)
